1e48c2d96790c2cfafa7ed152352872abecffad9
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-27 13:47:54 +0300

    how  work capacity
    I looked at how the memory allocation of vectors works on rust

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,21 @@ fn main() {
         println!("{:?}", addelementpush(&mut list, 3));
         println!("{:?}", addelementinsert(&mut list, 3, 0));
         getelement(&mut list, 3);
+        vector_capacity();
     } else {
         println!("If u're here it means something went wrong");
     }
+}
+fn vector_capacity(){
+    let mut list1:Vec<i32> = Vec::with_capacity(4);
+    println!("{}",list1.capacity());
+    list1.push(1);
+    list1.push(2);
+    list1.push(3);
+    list1.push(4);
+    list1.push(5);
+    list1.push(7);
+    println!("{}",list1.capacity());
 }
 
 fn gettingfirstelement(list: Vec<i32>) -> bool {//can write better function with true false but i want it do latter
