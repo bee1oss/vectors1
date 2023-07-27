4c80a3d71755175430d73a88f6e57893d3e97121
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-27 11:20:30 +0300

    cheking empty vector
    cheking our vector with function first , we are using this method for cheking our vector empty or not.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 fn main() {
     let mut list = vec![1, 2, 3, 4, 5, 6];
+    gettingfirstelement(list);
     println!("{:?}", addelementpush(&mut list, 3));
     println!("{:?}", addelementinsert(&mut list, 3, 0));
-    getelement(&mut list,3);
+    getelement(&mut list, 3);
+
+}
+
+fn gettingfirstelement(list: Vec<i32>) {
+    match list.first() {
+        Some(el) => println!("First element is {}", el),
+        None =>println!("Vector is empty!"),
+    }
 }
 
 fn getelement(list: &mut Vec<i32>, index: usize) {
     match list.get(index) {
         Some(el) => println!("Element is {}", el),
-        None => println!("")
+        None => println!("Element with index {} doesnt exist!", index)
     }
 }
 
