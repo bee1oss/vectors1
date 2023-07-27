7d6f1f4cb529c53cc5d4e486a7ca2e165e48227e
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-27 10:34:04 +0300

    correct func
    some sorrection in functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
     let mut list = vec![1, 2, 3, 4, 5, 6];
-    println!("{:?}",addelement(&mut list));
+    println!("{:?}",addelementpush(&mut list,3));
 
 }
 
-fn addelement(list: &mut Vec<i32>) -> &mut Vec<i32> {
-    list.push(1);
-    list.insert(2, 0);
+fn addelementpush(list: &mut Vec<i32>,value:i32) -> &mut Vec<i32> {
+    list.push(value);
+    return list;
+}
+fn addelementinsert(list: &mut Vec<i32>,index:usize,value:i32) -> &mut Vec<i32> {
+    list.insert(index, value);
     return list;
 }
